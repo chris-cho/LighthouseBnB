4d23431b1b928900de83ee5b26ecdda3f7afb752
Chris Cho <hyunsangc@gmail.com>
2021-10-14 23:21:56 -0400

    add property adding capability

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -166,9 +166,17 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+  return pool
+    .query(
+      `INSERT INTO properties (title, description, country, street, city, province, post_code, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, thumbnail_photo_url, cover_photo_url, owner_id)VALUES ($1, $2 ,$3 ,$4 ,$5 ,$6, $7, $8, $9, $10, $11, $12, $13, $14)RETURNING *;`, [property.title, property.description, property.country, property.street, property.city, property.province, property.post_code, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.thumbnail_photo_url, property.cover_photo_url, property.owner_id])
+      
+    .then((result) => {
+      console.log(result.rows);
+      return Promise.resolve(result.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
+};
 exports.addProperty = addProperty;
